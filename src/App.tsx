47b4764abdf0585f112b91dabe2e00a034b85522
Soehnle <lu-_-po@live.it>
2023-01-23 16:07:57 +0100

    pusho landing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Game from "./components/Game/Game";
 import Navbar from "./components/Navbar";
+import Landing from "./components/Landing"
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/game" element={<Game />} />
       </Routes>
