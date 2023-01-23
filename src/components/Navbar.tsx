7daa5d05fd01fcd35f5d8d8608a1d9d752c44053
PopsicK-frkr <patrizio.vela@gmail.com>
2023-01-23 06:25:30 +0100

    task fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,48 @@
-import React from "react";
-import "./Navbar.css";
-import ghost from "../img/ghost.png";
+import { useState } from "react";
+import { useEffect } from "react";
 import { navLinks } from "../utils";
 import { Link } from "react-router-dom";
+import ghost from "../img/ghost.png";
 
 const Navbar = () => {
-  const [clicked, setClicked] = React.useState(false);
+  const [clicked, setClicked] = useState(false);
 
   const handleClick = () => {
     setClicked(!clicked);
   };
 
+  const handleResize = () => {
+    if (window.innerWidth > 640) {
+      setClicked(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <nav className="flex items-center justify-between bg-[#1b2430] p-5 ">
         <div className="flex text-white">
-          <img
-            src={ghost}
-            alt="navbar ghost icon"
-            className="w-[90px] w-[60px]"
-          />
-          <h1 className="ml-4 text-4xl font-bold mt-4">Duck Souls</h1>
+          <img src={ghost} alt="navbar ghost icon" className="w-20 h-20" />
+          <h1 className="ml-1 text-3xl font-bold mt-6">Duck Souls</h1>
         </div>
         <div>
           <ul
             className={`${
               clicked
-                ? "hidden"
-                : "fixed top-32 -right-80 w-64 h-screen bg-[1b2430f5] pt-40 pl-10 transition-all transition-duration-300 transition-timing-ease-in-out"
-            } `}
+                ? "visible sm:hidden fixed top-32 right-0 -mt-4 pl-3 w-64 h-screen bg-[#1b2430] opacity-95"
+                : "hidden sm:flex justify-between"
+            }`}
           >
             {navLinks.map((link, i) => (
               <li key={i} className="px-4 sticky mb-4">
                 <Link to={link.path}>
-                  <div className="text-2xl font-medium text-white hover:text-[#00bcd4] hover:underline underline-offset-8 decoration-[#00bcd4] transition-all transition-duration-300 transition-timing-ease-in-out">
+                  <div className="text-xl font-medium text-white mt-6 hover:text-[#00bcd4] hover:underline underline-offset-8 decoration-[#00bcd4] transition-all transition-duration-300 transition-timing-ease-in-out">
                     {link.name}
                   </div>
                 </Link>
@@ -41,9 +50,9 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        <div className="md:hidden" onClick={handleClick}>
+        <div className="sm:hidden" onClick={handleClick}>
           <i
-            className={`text-white text-3xl cursor-pointer ${
+            className={`text-white text-2xl cursor-pointer ${
               clicked ? "fas fa-times" : "fas fa-bars"
             }`}
           ></i>
